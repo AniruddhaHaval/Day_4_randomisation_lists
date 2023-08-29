f496b26d68978d6784ec84b27f6c851cdfb8aad8
AniruddhaHaval <havalaniruddha@gmail.com>
2023-08-29 06:42:40 +0530

    corrected random.randint

--- banker_roulette.py
+++ banker_roulette.py
@@ -10,6 +10,6 @@ names_length = len(names_lists)
 
 print(names_length)
 
-print(f"Today {names_lists[names_length-1]} will pay the bill.")
+print(f"Today {names_lists[random.randint(0, (names_length-1))]} will pay the bill.")
 
 print(f"Today {random.choice(names_lists)} will pay the bill.")
