d2cec7c18c67a4f908829b7ea2598bddb44583f6
AniruddhaHaval <havalaniruddha@gmail.com>
2023-08-29 06:23:26 +0530

    list append extend complete

---
+++ lists_append_extend.py
@@ -0,0 +1,13 @@
+states_of_india = ["MH", "GJ", "UK"]
+
+print(states_of_india[2])
+
+states_of_india[2] = "NL"
+
+states_of_india.append("RJ")
+
+print(states_of_india)
+
+states_of_india.extend(["KL","TL"])
+
+print(states_of_india)
